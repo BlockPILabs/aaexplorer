ff4c806e24c38e2dbf875dbf45352a19aa3c6e4c
liwuv587 <liwuv587@gmail.com>
2023-09-04 16:49:18 +0800

    bash build

--- build/devlop/aws/build-send.sh
+++ build/devlop/aws/build-send.sh
@@ -6,7 +6,8 @@ cd ../../../
 pwd
 
 make build
-
+ssh root@18.211.227.200 systemctl stop aim
 scp ./dist/aim root@18.211.227.200:/blockpi/aa-scan/aim
+ssh root@18.211.227.200 systemctl start aim
 
 #./aim --home ./.aim start
