480aa97c0d6a836e4cb50f79b5caa4418968d9be
bozblockpi <boz@blockpi.io>
2023-09-05 16:19:56 +0800

    add statistic

--- explorer/router.go
+++ explorer/router.go
@@ -35,7 +35,7 @@ func Resister(router fiber.Router) {
 
 	//user
 	//networksV1.Get("/user")
-	networksV1.Get("/user")
+	//networksV1.Get("/user")
 
 	//home page
 	networksV1.Get("/dailyStatistic", controller.GetDailyStatistic).Name(controller.NameGetDailyStatistic)
