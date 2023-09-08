746565b7290b7dc1137ec05dc946e0788c70641b
liwuv587 <liwuv587@gmail.com>
2023-09-08 14:13:32 +0800

    userop list

--- build/devlop/aws/build-send.sh
+++ build/devlop/aws/build-send.sh
@@ -7,7 +7,9 @@ pwd
 
 make build
 ssh root@18.211.227.200 systemctl stop aim
+ssh root@18.211.227.200 systemctl stop aim-task
 scp ./dist/aim root@18.211.227.200:/blockpi/aa-scan/aim
 ssh root@18.211.227.200 systemctl start aim
+ssh root@18.211.227.200 systemctl start aim-task
 
 #./aim --home ./.aim start
