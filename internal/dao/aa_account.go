a29a5de8b45c73e8d8d769c804ef597ec5020230
liwuv587 <liwuv587@gmail.com>
2023-09-15 15:56:41 +0800

    search

--- internal/dao/aa_account.go
+++ internal/dao/aa_account.go
@@ -19,10 +19,7 @@ var AaAccountDao = &aaAccountDao{}
 
 func (dao *aaAccountDao) Search(ctx context.Context, tx *ent.Client, req vo.SearchAllRequest) (a ent.AaAccountDataSlice, err error) {
 
-	term := strings.ToLower(req.Term)
-	if !utils.Has0xPrefix(term) {
-		term = "0x" + term
-	}
+	term := utils.Fix0x(strings.ToLower(req.Term))
 
 	query := tx.AaAccountData.Query()
 	if utils.IsHexAddress(term) {
