4918f59fc03ce8a2d6996e959856f3d237c2c921
bozblockpi <boz@blockpi.io>
2023-09-16 14:15:55 +0800

    solve data problem

--- internal/entity/schema/aa_account_data.go
+++ internal/entity/schema/aa_account_data.go
@@ -2,9 +2,11 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/dialect"
 	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/field"
+	"github.com/shopspring/decimal"
 )
 
 type AaAccountData struct {
@@ -17,6 +19,10 @@ func (AaAccountData) Fields() []ent.Field {
 		field.String("aa_type").StructTag(`json:"aaType"`),
 		field.String("factory").StructTag(`json:"factory"`),
 		field.Time("factory_time").StructTag(`json:"factoryTime"`),
+		field.Int64("user_ops_num").StructTag(`json:"userOpsNum"`).Optional(),
+		field.Int64("total_balance_usd").StructTag(`json:"totalBalanceUsd"`).GoType(decimal.Zero).
+			SchemaType(map[string]string{dialect.Postgres: "numeric(50, 20)"}).Optional(),
+		field.Int64("last_time").StructTag(`json:"lastTime"`).Optional(),
 		field.Time("update_time").StructTag(`json:"updateTime"`).Optional(),
 	}
 }
