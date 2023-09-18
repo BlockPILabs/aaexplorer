6c2968db1392ed59b2446bce7de0b6ce1f21b7da
liwuv587 <liwuv587@gmail.com>
2023-09-18 15:52:59 +0800

    tx search

--- cmd/aim/commands/function_signature.go
+++ cmd/aim/commands/function_signature.go
@@ -1,10 +1,17 @@
 package commands
 
 import (
+	"encoding/json"
 	"github.com/BlockPILabs/aa-scan/internal/entity"
+	"github.com/BlockPILabs/aa-scan/internal/entity/ent"
+	"github.com/BlockPILabs/aa-scan/internal/entity/ent/functionsignature"
 	"github.com/BlockPILabs/aa-scan/internal/memo"
+	out_service "github.com/BlockPILabs/aa-scan/service"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cobra"
+	"github.com/valyala/fastjson"
+	"strings"
+	"time"
 )
 
 var FunctionSignatureCmd = &cobra.Command{
@@ -23,7 +30,7 @@ var FunctionSignatureCmd = &cobra.Command{
 		}
 
 		nextUrl := "https://www.4byte.directory/api/v1/signatures/?format=json"
-		for true {
+		for len(nextUrl) > 0 {
 			(func() {
 				agent := fiber.AcquireAgent()
 				defer fiber.ReleaseAgent(agent)
@@ -31,8 +38,77 @@ var FunctionSignatureCmd = &cobra.Command{
 				request := agent.Request()
 				request.Header.SetMethod(fiber.MethodGet)
 				request.SetRequestURI(nextUrl)
+				err := agent.Parse()
+				nextUrl = ""
+				if err != nil {
+					logger.Error("agent parse error", "err", err)
+					return
+				}
+				code, bytes, errs := agent.Bytes()
+				if errs != nil || code != fiber.StatusOK {
+					logger.Error("agent request error", "status", code, "errs", errs)
+					return
+				}
+				value, err := fastjson.ParseBytes(bytes)
+				if err != nil {
+					logger.Error("json parse error", "err", err)
+					return
+				}
+				nextUrl = strings.Replace(string(value.GetStringBytes("next")), "http:", "https:", 1)
+				values := value.GetArray("results")
+				if len(values) < 1 {
+					logger.Error("data not found")
+					return
+				}
+				client, _ := entity.Client(cmd.Context())
+				var ids []string
+				var fs []*out_service.FunctionSignature
+				for _, v := range values {
+					bytes = v.MarshalTo(nil)
+					f := &out_service.FunctionSignature{}
+					err = json.Unmarshal(bytes, f)
+					if err != nil {
+						logger.Error("json parse error 1", "err", err)
+						continue
+					}
+					if len(f.TextSignature) > 0 {
+						ss := strings.Split(f.TextSignature, "(")
+						f.Name = ss[0]
+						fs = append(fs, f)
+						ids = append(ids, f.HexSignature)
+					}
 
-				agent.Parse()
+				}
+
+				if len(ids) < 1 {
+					return
+				}
+
+				functionSignatures := client.FunctionSignature.Query().Where(
+					functionsignature.IDIn(ids...),
+				).AllX(cmd.Context())
+
+				sfm := map[string]*ent.FunctionSignature{}
+				for _, sf := range functionSignatures {
+					sfm[sf.ID] = sf
+				}
+
+				bluk := []*ent.FunctionSignatureCreate{}
+				for _, f := range fs {
+					if _, ok := sfm[f.HexSignature]; ok {
+						continue
+					}
+					bluk = append(bluk,
+						client.FunctionSignature.Create().
+							SetID(f.HexSignature).
+							SetName(f.Name).
+							SetText(f.TextSignature).
+							SetBytes(f.BytesSignature).
+							SetCreateTime(time.Now()),
+					)
+				}
+
+				client.FunctionSignature.CreateBulk(bluk...).ExecX(cmd.Context())
 
 			})()
 		}
