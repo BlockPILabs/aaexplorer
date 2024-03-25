3c076bf476c076f28ec26fd4a6191403b23fabb6
liwu <liwuv587@gmail.com>
2024-03-25 17:10:56 +0800

    block sync

--- task/block_scan_task.go
+++ task/block_scan_task.go
@@ -2,10 +2,11 @@ package task
 
 import (
 	"context"
-	"fmt"
-	"github.com/BlockPILabs/aaexplorer/internal/dao"
 	"github.com/BlockPILabs/aaexplorer/internal/entity"
+	"github.com/BlockPILabs/aaexplorer/internal/log"
 	"github.com/BlockPILabs/aaexplorer/third/schedule"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"time"
 )
 
 func init() {
@@ -19,16 +20,50 @@ func init() {
 }
 
 func BlockSyncRun(ctx context.Context) {
-	logger.Info("start", "nets", config.Task.Networks)
+	log.Context(ctx).Info("start", "nets", config.Task.Networks)
 	tx, err := entity.Client(ctx)
 	if err != nil {
+		log.Context(ctx).Error("db connect error", "err", err)
 		return
 	}
-	networks, err := dao.NetworkDao.GetNetworks(ctx, tx)
+
+	networks, err := getTaskNetworks(ctx, tx)
 	if err != nil {
+		log.Context(ctx).Error("error in getTaskNetworks", "err", err)
 		return
 	}
-	fmt.Sprintln(networks)
+
+	for _, network := range networks {
+		ctx, logger := log.With(ctx, "network", network.ID, "networkName", network.Name)
+
+		dialContext, err := ethclient.DialContext(ctx, network.HTTPRPC)
+		if err != nil {
+			logger.Error("error in getTaskNetworks", "err", err)
+			continue
+		}
+		blockNumber, err := dialContext.BlockNumber(ctx)
+		if err != nil {
+			logger.Error("error in BlockNumber", "err", err)
+			continue
+		}
+
+		networkTx, err := entity.Client(ctx, network.ID)
+		if err != nil {
+			logger.Error("error in network db connect", "err", err)
+			return
+		}
+
+		result, err := networkTx.ExecContext(ctx, `insert into block_sync(block_num, create_time, update_time) select generate_series(max(block_num) , $1 ) , $2 , $2 from block_sync on conflict do nothing`, blockNumber, time.Now())
+		if err != nil {
+			logger.Error("error in block_sync generate_series", "err", err)
+			continue
+		}
+		rowsAffected, _ := result.RowsAffected()
+
+		logger.Info("block sync result", "rowsAffected", rowsAffected)
+
+	}
+
 }
 
 func BlockScanRun(ctx context.Context) {
