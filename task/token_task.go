42ec4b7ae7df7563dd1adca18ce8376c525700c6
zhaochengpro <147230368+NeganZhao@users.noreply.github.com>
2024-03-27 19:04:07 +0800

    update token top 300 task

--- task/token_task.go
+++ task/token_task.go
@@ -2,35 +2,42 @@ package task
 
 import (
 	"context"
+	constConfig "github.com/BlockPILabs/aaexplorer/config"
 	"github.com/BlockPILabs/aaexplorer/internal/entity"
 	"github.com/BlockPILabs/aaexplorer/internal/entity/ent/token"
 	"github.com/BlockPILabs/aaexplorer/third/cmc"
+	"github.com/BlockPILabs/aaexplorer/third/schedule"
 	"github.com/shopspring/decimal"
 	"time"
 )
-import constConfig "github.com/BlockPILabs/aaexplorer/config"
+
+func init() {
+	schedule.Add("token_task", func(ctx context.Context) {
+		TokenTask(ctx)
+	}).ScheduleWithCron("0 0 0 * * *")
+}
 
 func TokenTask(ctx context.Context) {
-	tokenInfos := cmc.GetTopToken(constConfig.TopNum)
-	if len(tokenInfos) == 0 {
+	tokenList := cmc.GetTopToken(constConfig.TopNum)
+	if len(tokenList) == 0 {
 		return
 	}
 	cli, err := entity.Client(ctx)
 	if err != nil {
 		return
 	}
+
 	networks, err := cli.Network.Query().All(ctx)
-	if len(networks) == 0 {
-		return
-	}
+
 	for _, net := range networks {
 		network := net.ID
 		client, err := entity.Client(ctx, network)
 		if err != nil {
 			continue
 		}
-		for _, tokenInfo := range tokenInfos {
-			symbol := tokenInfo.Symbol
+
+		for _, tokenItem := range tokenList {
+			symbol := tokenItem.Symbol
 			tokens, err := client.Token.Query().Where(token.SymbolEqualFold(symbol), token.NetworkEqualFold(network)).All(ctx)
 			if err != nil {
 				logger.Error("TokenTask get token err ", "symbol", "msg", symbol, err)
@@ -39,24 +46,22 @@ func TokenTask(ctx context.Context) {
 			if len(tokens) > 0 {
 				continue
 			}
-
-			contractAddress := ""
-			platform := tokenInfo.Platform
-			if platform != nil && platform.Slug == network {
-				contractAddress = platform.TokenAddress
+			var contractAddress string
+			platfrom := tokenItem.Platform
+			if platfrom != nil && platfrom.Slug == network {
+				contractAddress = platfrom.TokenAddress
 			}
+
 			now := time.Now()
 			_, err = client.Token.Create().SetTokenPrice(decimal.Zero).
-				SetNetwork(constConfig.EthNetwork).SetUpdateTime(now).SetCreateTime(now).SetLastTime(now.UnixMilli()).SetSymbol(symbol).
-				SetContractAddress(contractAddress).SetMarketRank(tokenInfo.Rank).SetFullName(tokenInfo.Name).SetType("").Save(ctx)
-
+				SetNetwork(network).SetUpdateTime(now).SetCreateTime(now).SetLastTime(now.UnixMilli()).
+				SetSymbol(symbol).SetContractAddress(contractAddress).SetMarketRank(tokenItem.Rank).SetFullName(tokenItem.Name).
+				SetType("").Save(ctx)
 			if err != nil {
 				logger.Error("TokenTask save token err ", "symbol", "msg", symbol, err)
 			} else {
 				logger.Info("TokenTask save token success", "symbol", symbol)
 			}
-
 		}
 	}
-
 }
