5ce9a3f4948155b8b69208479316f2ad42dae8c2
bozblockpi <boz@blockpi.io>
2024-03-29 18:30:22 +0800

    aa whale

--- task/token_task.go
+++ task/token_task.go
@@ -5,10 +5,14 @@ import (
 	"github.com/BlockPILabs/aaexplorer/internal/entity"
 	"github.com/BlockPILabs/aaexplorer/internal/entity/ent/token"
 	"github.com/BlockPILabs/aaexplorer/third/cmc"
+	"github.com/procyon-projects/chrono"
 	"github.com/shopspring/decimal"
+	"log"
 	"time"
 )
 import constConfig "github.com/BlockPILabs/aaexplorer/config"
+
+const PriceExpire = 20 * 3600 * 1000
 
 func TokenTask(ctx context.Context) {
 	tokenInfos := cmc.GetTopToken(constConfig.TopNum)
@@ -56,6 +60,117 @@ func TokenTask(ctx context.Context) {
 				logger.Info("TokenTask save token success", "symbol", symbol)
 			}
 
+		}
+	}
+
+}
+
+func InitRefreshToken(ctx context.Context) {
+	hourScheduler := chrono.NewDefaultTaskScheduler()
+	_, err := hourScheduler.ScheduleWithCron(func(ctx context.Context) {
+		RefreshToken(ctx)
+	}, "0 1 1 * * *")
+
+	if err == nil {
+		log.Print("RefreshToken has been scheduled")
+	}
+}
+
+func InitRefreshPrice(ctx context.Context) {
+	hourScheduler := chrono.NewDefaultTaskScheduler()
+	_, err := hourScheduler.ScheduleWithCron(func(ctx context.Context) {
+		RefreshPrice(ctx)
+	}, "0 1 2 * * *")
+
+	if err == nil {
+		log.Print("RefreshPrice has been scheduled")
+	}
+}
+
+func RefreshToken(ctx context.Context) {
+	tokenInfos := cmc.GetTopToken(constConfig.TopNum)
+	if len(tokenInfos) == 0 {
+		return
+	}
+	cli, err := entity.Client(ctx)
+	if err != nil {
+		return
+	}
+	networks, err := cli.Network.Query().All(ctx)
+	if len(networks) == 0 {
+		return
+	}
+	for _, net := range networks {
+		network := net.ID
+		client, err := entity.Client(ctx, network)
+		if err != nil {
+			continue
+		}
+		for _, tokenInfo := range tokenInfos {
+			symbol := tokenInfo.Symbol
+			tokens, err := client.Token.Query().Where(token.SymbolEqualFold(symbol), token.NetworkEqualFold(network)).All(ctx)
+			if err != nil {
+				logger.Error("RefreshToken get token err ", "symbol", "msg", symbol, err)
+				continue
+			}
+			if len(tokens) > 0 {
+				continue
+			}
+
+			contractAddress := ""
+			platform := tokenInfo.Platform
+			if platform != nil && platform.Slug == network {
+				contractAddress = platform.TokenAddress
+			}
+			now := time.Now()
+			_, err = client.Token.Create().SetTokenPrice(decimal.Zero).
+				SetNetwork(network).SetUpdateTime(now).SetCreateTime(now).SetLastTime(now.UnixMilli()).SetSymbol(symbol).
+				SetContractAddress(contractAddress).SetMarketRank(tokenInfo.Rank).SetFullName(tokenInfo.Name).SetType("").Save(ctx)
+
+			if err != nil {
+				logger.Error("RefreshToken save token err ", "symbol", "msg", symbol, err)
+			} else {
+				logger.Info("RefreshToken save token success", "symbol", symbol)
+			}
+
+		}
+	}
+}
+
+func RefreshPrice(ctx context.Context) {
+	cli, err := entity.Client(ctx)
+	if err != nil {
+		return
+	}
+	networks, err := cli.Network.Query().All(ctx)
+	if len(networks) == 0 {
+		return
+	}
+	for _, net := range networks {
+		network := net.ID
+		client, err := entity.Client(ctx, network)
+		if err != nil {
+			continue
+		}
+		tokens, err := client.Token.Query().Where(token.NetworkEqualFold(network)).All(ctx)
+		if len(tokens) == 0 {
+			continue
+		}
+		now := time.Now().UnixMilli()
+		for _, one := range tokens {
+			if now-one.LastTime < PriceExpire {
+				continue
+			}
+			one.Symbol = "REI"
+			price := cmc.GetTokenPrice(one.Symbol)
+			err := client.Token.Update().SetTokenPrice(price).SetLastTime(now).Where(token.IDEQ(one.ID)).Exec(ctx)
+			if err != nil {
+				logger.Error("RefreshTokenPrice err ", "symbol", one.Symbol, "msg", err)
+			} else {
+				logger.Error("RefreshTokenPrice success ", "symbol", one.Symbol, "msg", err)
+
+			}
+			time.Sleep(time.Second)
 		}
 	}
 
--- third/cmc/cmc_api.go
+++ third/cmc/cmc_api.go
@@ -3,6 +3,7 @@ package cmc
 import (
 	"encoding/json"
 	interlog "github.com/BlockPILabs/aaexplorer/internal/log"
+	"github.com/shopspring/decimal"
 	"io"
 	"net/http"
 	"strconv"
@@ -10,6 +11,7 @@ import (
 
 const CmcUrl = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?sort=cmc_rank"
 const CmcAuthVal = "e7f31052-0284-4ad6-8115-e35f2ecc8028"
+const CmcPriceUrl = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?"
 
 var logger = interlog.L()
 
@@ -77,5 +79,87 @@ func GetTopToken(num int64) []TokenInfo {
 	}
 
 	return tokenResp.Data
+
+}
+
+type TokenPriceResp struct {
+	Data   map[string][]PriceDataDetail `json:"data"`
+	Status *PriceStatus                 `json:"status"`
+}
+
+type PriceStatus struct {
+	Timestamp    string `json:"timestamp"`
+	ErrorCode    int64  `json:"error_code"`
+	ErrorMessage string `json:"error_message"`
+	Elapsed      int64  `json:"elapsed"`
+	CreditCount  int64  `json:"credit_count"`
+	Notice       string `json:"notice"`
+}
+
+type PriceData struct {
+	Key1 *PriceDataDetail `json:"1"`
+}
+
+type PriceDataDetail struct {
+	Id      int64  `json:"id"`
+	Name    string `json:"name"`
+	Symbol  string `json:"symbol"`
+	Slug    string `json:"slug"`
+	CmcRank int64  `json:"cmc_rank"`
+	Quote   *Quote `json:"quote"`
+}
+
+type Quote struct {
+	USD *QuoteUsd `json:"USD"`
+}
+
+type QuoteUsd struct {
+	Price            decimal.Decimal `json:"price"`
+	Volume24h        decimal.Decimal `json:"volume_24h"`
+	PercentChange24h decimal.Decimal `json:"percent_change_24h"`
+}
+
+func GetTokenPrice(symbol string) decimal.Decimal {
+	url := CmcPriceUrl
+
+	req, _ := http.NewRequest("GET", url+"symbol="+symbol, nil)
+
+	req.Header.Add("X-CMC_PRO_API_KEY", CmcAuthVal)
+
+	res, _ := http.DefaultClient.Do(req)
+	if res == nil {
+		logger.Info("GetTokenPrice err, ")
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	bodyData := string(body)
+
+	var tokenPriceResp *TokenPriceResp
+
+	err := json.Unmarshal([]byte(bodyData), &tokenPriceResp)
+	if err != nil {
+		logger.Error("GetTopToken parse err ", "msg", err)
+		return decimal.Zero
+	}
+	var price = decimal.Zero
+	if tokenPriceResp != nil {
+		data := tokenPriceResp.Data
+		if len(data) > 0 {
+			priceDataDetails := data[symbol]
+			if len(priceDataDetails) > 0 {
+				quote := priceDataDetails[0].Quote
+				if quote != nil {
+					usd := quote.USD
+					if usd != nil {
+						price = usd.Price
+					}
+				}
+			}
+		}
+	}
+
+	return price
 
 }
