8b0c746efc88cfcede6f7195cacefdb3ee58c08a
bozblockpi <boz@blockpi.io>
2024-04-08 17:19:01 +0800

    aa whale

--- task/asset_refresh_task.go
+++ task/asset_refresh_task.go
@@ -20,7 +20,7 @@ import (
 const Abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
 
 func InitAssetRefreshTask() {
-	//go AssetRefreshTask(context.Background())
+	go AssetRefreshTask(context.Background())
 	hourScheduler := chrono.NewDefaultTaskScheduler()
 	_, err := hourScheduler.ScheduleWithCron(func(ctx context.Context) {
 		AssetRefreshTask(ctx)
