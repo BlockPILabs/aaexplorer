6b75ffbcb96d7e9bbce72dd7c6c78d916e6905c6
bozblockpi <boz@blockpi.io>
2024-05-20 17:17:25 +0800

    mev

--- build/devlop/aws-prod/build-send.sh
+++ build/devlop/aws-prod/build-send.sh
@@ -19,21 +19,21 @@ version="${version_num}.${COMMIT_HASH}"
 
 make build
 
-ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com mkdir -p /blockpi/aaexplorer/log/ || exit 1
-ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com mkdir -p ${version_dir} || exit 1
-scp -o StrictHostKeyChecking=no  ./dist/aim root@ec2-54-85-44-73.compute-1.amazonaws.com:"${version_dir}/${version}" || exit 1
-ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com ln -f -s ${version_dir}/${version} /blockpi/aaexplorer/aim || exit 1
+#ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com mkdir -p /blockpi/aaexplorer/log/ || exit 1
+#ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com mkdir -p ${version_dir} || exit 1
+#scp -o StrictHostKeyChecking=no  ./dist/aim root@ec2-54-85-44-73.compute-1.amazonaws.com:"${version_dir}/${version}" || exit 1
+#ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com ln -f -s ${version_dir}/${version} /blockpi/aaexplorer/aim || exit 1
 
-ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com supervisorctl restart aim
+#ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com supervisorctl restart aim
 #ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com supervisorctl start aim-task
 #ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com systemctl start aim
 #ssh -o StrictHostKeyChecking=no  root@ec2-54-85-44-73.compute-1.amazonaws.com systemctl start aim-task
 
 #new db
-#ssh -o StrictHostKeyChecking=no  root@65.108.127.107 mkdir -p /blockpi/aaexplorer/log/ || exit 1
-#ssh -o StrictHostKeyChecking=no  root@65.108.127.107 mkdir -p ${version_dir} || exit 1
-#scp -o StrictHostKeyChecking=no  ./dist/aim root@65.108.127.107:"${version_dir}/${version}" || exit 1
-#ssh -o StrictHostKeyChecking=no  root@65.108.127.107 ln -f -s ${version_dir}/${version} /blockpi/aaexplorer/aim || exit 1
+ssh -o StrictHostKeyChecking=no  root@162.55.130.133 mkdir -p /blockpi/aaexplorer/log/ || exit 1
+ssh -o StrictHostKeyChecking=no  root@162.55.130.133 mkdir -p ${version_dir} || exit 1
+scp -o StrictHostKeyChecking=no  ./dist/aim root@162.55.130.133:"${version_dir}/${version}" || exit 1
+ssh -o StrictHostKeyChecking=no  root@162.55.130.133 ln -f -s ${version_dir}/${version} /blockpi/aaexplorer/aim || exit 1
 
-#ssh -o StrictHostKeyChecking=no  root@65.108.127.107 supervisorctl restart aim
+ssh -o StrictHostKeyChecking=no  root@162.55.130.133 supervisorctl restart aim
 #./aim --home ./.aim start
--- build/devlop/aws/build-send.sh
+++ build/devlop/aws/build-send.sh
@@ -8,13 +8,21 @@ set -x
 make build
 #ssh root@18.211.227.200 systemctl stop aim
 #ssh root@18.211.227.200 systemctl stop aim-task
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim-task
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 mkdir -p /blockpi/aaexplorer/log/
-scp ./dist/aim root@18.211.227.200:/blockpi/aaexplorer/aim
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim-task
+
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim-task
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 mkdir -p /blockpi/aaexplorer/log/
+#scp ./dist/aim root@18.211.227.200:/blockpi/aaexplorer/aim
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim-task
+
 #ssh root@18.211.227.200 systemctl start aim
 #ssh root@18.211.227.200 systemctl start aim-task
+ssh -o StrictHostKeyChecking=no root@162.55.130.133 supervisorctl stop aim
+ssh -o StrictHostKeyChecking=no root@162.55.130.133 supervisorctl stop aim-task
+ssh -o StrictHostKeyChecking=no root@162.55.130.133 mkdir -p /blockpi/aaexplorer/log/
+scp ./dist/aim root@162.55.130.133:/blockpi/aaexplorer/aim
+ssh -o StrictHostKeyChecking=no root@162.55.130.133 supervisorctl start aim
+ssh -o StrictHostKeyChecking=no root@162.55.130.133 supervisorctl start aim-task
 
 #./aim --home ./.aim start
