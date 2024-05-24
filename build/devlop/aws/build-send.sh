1b63b9bb80ea78f74cad1340cdece94b7c882b46
bozblockpi <boz@blockpi.io>
2024-05-24 14:34:47 +0800

    monitor

--- build/devlop/aws/build-send.sh
+++ build/devlop/aws/build-send.sh
@@ -9,12 +9,12 @@ make build
 #ssh root@18.211.227.200 systemctl stop aim
 #ssh root@18.211.227.200 systemctl stop aim-task
 
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim-task
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 mkdir -p /blockpi/aaexplorer/log/
-scp ./dist/aim root@18.211.227.200:/blockpi/aaexplorer/aim
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim
-ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim-task
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl stop aim-task
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 mkdir -p /blockpi/aaexplorer/log/
+#scp ./dist/aim root@18.211.227.200:/blockpi/aaexplorer/aim
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim
+#ssh -o StrictHostKeyChecking=no root@18.211.227.200 supervisorctl start aim-task
 
 #ssh root@18.211.227.200 systemctl start aim
 #ssh root@18.211.227.200 systemctl start aim-task
