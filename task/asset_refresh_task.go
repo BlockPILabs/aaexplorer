4c008d18dcc5cc974bd45be798050165898f3107
bozblockpi <boz@blockpi.io>
2024-05-24 15:27:04 +0800

    monitor

--- task/asset_refresh_task.go
+++ task/asset_refresh_task.go
@@ -22,9 +22,10 @@ import (
 const Abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
 
 func InitAssetRefreshTask() {
+	go AssetRefreshTask(context.Background())
 	hourScheduler := chrono.NewDefaultTaskScheduler()
 	_, err := hourScheduler.ScheduleWithCron(func(ctx context.Context) {
-		AssetRefreshTask(ctx)
+		//AssetRefreshTask(ctx)
 	}, "0 35 0 1/6 * *")
 
 	if err == nil {
