240e782a8faeb669be2f467e9a64025c727aa546
bozblockpi <boz@blockpi.io>
2024-05-27 14:53:21 +0800

    monitor

--- task/top_paymaster_task.go
+++ task/top_paymaster_task.go
@@ -16,8 +16,8 @@ import (
 
 func TopPaymaster() {
 
-	go doTopPaymasterDay()
-	go doTopPaymasterHour(1)
+	//go doTopPaymasterDay()
+	//go doTopPaymasterHour(1)
 
 	paymasterScheduler := chrono.NewDefaultTaskScheduler()
 	_, err := paymasterScheduler.ScheduleWithCron(func(ctx context.Context) {
@@ -55,7 +55,7 @@ func doTopPaymasterDay() {
 			continue
 		}
 		now := time.Now()
-		startTime := time.Date(now.Year(), now.Month(), now.Day()-700, 0, 0, 0, 0, now.Location())
+		startTime := time.Date(now.Year(), now.Month(), now.Day()-1, 0, 0, 0, 0, now.Location())
 		endTime := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 		paymasterStatisDays, err := client.PaymasterStatisDay.
 			Query().
