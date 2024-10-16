828d3b419b6e61adc9d52a84971954c7a57c2afb
bozblockpi <boz@blockpi.io>
2024-10-16 10:38:34 +0800

    optimization

--- task/transfer_task.go
+++ task/transfer_task.go
@@ -87,9 +87,6 @@ func handlerOneNetwork(ctx context.Context, network string, net *ent.Network) {
 	if err != nil {
 		return
 	}
-	if network == "optimism" {
-		lastBlockNum = int64(126723592)
-	}
 	//} else if network == "taiko-hekla" {
 	//	lastBlockNum = int64(873611)
 	//}
